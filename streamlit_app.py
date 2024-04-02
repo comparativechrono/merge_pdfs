7c79682a0f4eb81cf64f3322f8220e790034061a
comparativechrono <80172051+comparativechrono@users.noreply.github.com>
2024-04-02 22:00:21 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,15 @@
+pip install PyPDF2
 import streamlit as st
 import PyPDF2
 from io import BytesIO
+
+st.title('📚 PDF Merger Tool')
+
+st.markdown("""
+Merge your PDF documents into one file easily. Just upload your PDF files in the order you want them merged.
+""")
+
+uploaded_files = st.file_uploader("Choose PDF files", accept_multiple_files=True, type='pdf', help="You can select multiple files by holding down the 'Shift' or 'Ctrl' (Command on Mac) key while selecting.")
 
 def merge_pdfs(paths):
     pdf_writer = PyPDF2.PdfWriter()
@@ -10,14 +19,13 @@ def merge_pdfs(paths):
             pdf_writer.add_page(reader.pages[page])
     return pdf_writer
 
-st.title('PDF Merger')
-
-uploaded_files = st.file_uploader("Choose PDF files", accept_multiple_files=True, type='pdf')
 if uploaded_files:
-    merged_pdf = merge_pdfs(uploaded_files)
-    with BytesIO() as output_bytes:
-        merged_pdf.write(output_bytes)
-        st.download_button(label="Download Merged PDF",
-                           data=output_bytes.getvalue(),
-                           file_name="merged_file.pdf",
-                           mime="application/pdf")
+    with st.spinner('Merging your PDFs...'):
+        merged_pdf = merge_pdfs(uploaded_files)
+        with BytesIO() as output_bytes:
+            merged_pdf.write(output_bytes)
+            st.success('Your PDFs have been successfully merged!')
+            st.download_button(label="📥 Download Merged PDF",
+                               data=output_bytes.getvalue(),
+                               file_name="merged_file.pdf",
+                               mime="application/pdf")
